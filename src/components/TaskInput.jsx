3ac3ed43c7cab68c853ff39c4dd4faaffa36943a
Iqra327 <iqrarasheed219@gmail.com>
2024-11-26 21:10:12 +0500

    Add taskInput component for adding tasks.

--- src/components/TaskInput.jsx
+++ src/components/TaskInput.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from './Button'
 
 const TaskInput = () => {
+  const [input, setInput] = useState('');
+  console.log(input);
+  
   return (
-    <div>TaskInput</div>
+    <div className='taskInput-div'>
+      <input
+        type='text'
+        placeholder='Enter Task'
+        className='taskInput'
+        value={input}
+        onChange={(e) => setInput(e.target.value)} 
+      />
+      <Button>Add Task</Button>
+    </div>
   )
 }
 
