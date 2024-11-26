165db8e6f31d58e6dfbf0b5fdb37f4266141a22b
Iqra327 <iqrarasheed219@gmail.com>
2024-11-26 23:05:23 +0500

    implement task deletion functionality

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -3,14 +3,24 @@ import { Tasks } from '../context/Context'
 import Button from './Button';
 
 const TaskList = () => {
-  const {allTasks} = useContext(Tasks);
+  const {allTasks, setAllTasks} = useContext(Tasks);
+  
+  const deleteTask = (id) => {
+    const updatedTasks =  allTasks.filter((task) => (
+       task.id !== id
+    ))
+    setAllTasks(updatedTasks)
+  }
 
   return (
     <div className='all-tasks-display'>
       {allTasks?.map((task) => (
         <div key={task.id} className='task-div'>
           <p>{task.text}</p>
-          <Button className='btn-delete-task'>
+          <Button 
+            className='btn-delete-task' 
+            onClick={() => deleteTask(task.id)}
+          >
             Delete
           </Button>
         </div>
